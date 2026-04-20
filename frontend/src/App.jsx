b84aee7e60b8d64e86315deff6c9581073c4e8aa
vignesh <vignesh@vigneshs-MacBook-Air.local>
2026-04-20 23:15:14 +0530

    force update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect, useRef } from "react";
 import axios from "axios";
 
-const API = "https://smart-notes-ai.onrender.com/";
+const API = "https://smart-notes-ai.onrender.com";
 
 // typing effect
 const typeText = (text, setMessages) => {
